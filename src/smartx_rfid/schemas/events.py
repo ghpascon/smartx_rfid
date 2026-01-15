aeb0fda363003a00cfe45752c6c194006f692a0e
Gabriel Henrique Pascon <gh.pascon@gmail.com>
2026-01-15 17:39:09 -0300

    fix event schema

--- src/smartx_rfid/schemas/events.py
+++ src/smartx_rfid/schemas/events.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
-
+from typing import Any
 
 class EventSchema(BaseModel):
     event_type: str = Field("event", description="Type of the event")
-    event_data = Field(None, description="Associated data with the event")
+    event_data: Any = Field(None, description="Associated data with the event")
