fa2b8fb437d8836deee33e000cc943048957e7f9
Gabriel Henrique Pascon <gh.pascon@gmail.com>
2026-01-16 17:31:39 -0300

    fix integration

--- src/smartx_rfid/webhook/xtrack.py
+++ src/smartx_rfid/webhook/xtrack.py
@@ -9,9 +9,14 @@ class WebhookXtrack:
 
     async def post(self, tag: dict):
         try:
+            device = tag.get("device", "unknown")
+            ant = tag.get("ant", "1")
+            epc = tag.get("epc", None)
+            if epc is None:
+                raise Exception("EPC is required")
             payload = f"""<msg>
                         <command>ReportRead</command>
-                        <data>EVENT=|DEVICENAME={tag.get("device", "")}|ANTENNANAME={tag.get("ant", "")}|TAGID={tag.get("epc", "")}|</data>
+                        <data>EVENT=|DEVICENAME={device}|ANTENNANAME={ant}|TAGID={epc}|</data>
                         <cmpl>STATE=|DATA1=|DATA2=|DATA3=|DATA4=|DATA5=|</cmpl>
                         </msg>"""
             async with httpx.AsyncClient() as client:
