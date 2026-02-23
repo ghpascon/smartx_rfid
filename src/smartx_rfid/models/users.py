0c267f330a0c8438b4a91954abce821abc2b404d
Gabriel Henrique Pascon <gh.pascon@gmail.com>
2026-02-23 08:29:09 -0300

    Id On Users

--- src/smartx_rfid/models/users.py
+++ src/smartx_rfid/models/users.py
@@ -5,14 +5,14 @@ Defines the Tag and Event models for storing RFID reader data
 with proper indexing and relationships.
 """
 
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Integer
 
 from .mixin import Base, BaseMixin
 
 
 class Users(Base, BaseMixin):
     __tablename__ = "users"
-
+    id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(String(100), nullable=False, index=True, unique=True)
     password_hash = Column(String(255), nullable=False)
     role = Column(String(50), nullable=False, default="user", index=True)
